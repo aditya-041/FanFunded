68afcf390dadb9ce6465a7bb10edead2240f70fd
aditya-041 <basuaditya260@gmail.com>
2024-07-25 20:33:59 +0530

    Fixed the stuff

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['c10.patreonusercontent.com', 'cdn.britannica.com'], // Add any other domains as needed
+    },
+};
+
+module.exports = nextConfig;
